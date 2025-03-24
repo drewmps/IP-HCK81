4dc29f1f17734ff47689eaa4d760887252f3f2e4
drewmps <andrewmarco97@gmail.com>
2025-03-24 20:32:01 +0700

    feat: membuat seeding

---
+++ server/seeders/20250324132026-seed-All.js
@@ -0,0 +1,22 @@
+"use strict";
+const { hashPassword } = require("../helpers/bcrypt");
+
+const fs = require("fs").promises;
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    let rows = JSON.parse(await fs.readFile("./data/users.json", "utf8"));
+    rows = rows.map((row) => {
+      delete row.id;
+      row.password = hashPassword(row.password);
+      row.createdAt = new Date();
+      row.updatedAt = new Date();
+      return row;
+    });
+    await queryInterface.bulkInsert("Users", rows);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
