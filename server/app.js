c6a7c7d9bdf5e565d49e2b6abaeff334fbe0bb67
drewmps <andrewmarco97@gmail.com>
2025-03-24 21:17:41 +0700

    feat: setup untuk server

---
+++ server/app.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const express = require("express");
+const UserController = require("./controllers/UserController");
+const errorHandler = require("./middlewares/errorHandler");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(errorHandler);
+
+module.exports = app;
